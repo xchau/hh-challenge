1a5773465b46c7b385e0d9c07d14c217ea2266c0
Minh Chau <xiu.chau@gmail.com>
2017-09-17 13:23:36 -0700

    create topnav component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import './styles/app.css';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import { TopNav } from './components/TopNav';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      colors: null
+    };
+  }
+
   render() {
     return (
       <div className="app-container">
-        <p className="app-test">hi</p>
+        <TopNav />
+        <Router>
+          
+        </Router>
       </div>
     );
   }
---
+++ src/components/TopNav.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../styles/topnav.css';
+import logo from '../assets/images/logo-symbol.svg';
+
+const TopNav = (props) => {
+  return (
+    <nav className="topnav-container">
+      <div className="topnav-content">
+        <img
+          alt="logo"
+          className="topnav-logo"
+          src={logo}
+        />
+        <input
+          className="topnav-searchbar"
+          placeholder="Search"
+          type="text"
+        />
+      </div>
+    </nav>
+  );
+};
+
+export { TopNav };
