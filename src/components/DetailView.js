54fee653f6871b69dda3fc7d5c65634ea465a643
Minh Chau <xiu.chau@gmail.com>
2017-09-21 10:04:07 -0700

    add tints to detail view

--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -2,11 +2,31 @@ import React from 'react';
 
 import { Swatch } from './Swatch';
 
+import { hexToRgb, generateTint } from '../utils/helpers.js';
+
 const DetailView = (props) => {
+  const rgb = hexToRgb(props.color);
+  const tints = [];
+
+  tints[0] = generateTint(rgb, -40);
+  tints[1] = generateTint(rgb, -20);
+  tints[2] = generateTint(rgb, 0);
+  tints[3] = generateTint(rgb, 20);
+  tints[4] = generateTint(rgb, 40);
+
   return (
     <div className="display-detail-container">
       <div className="swatch-large">
         <Swatch hex={props.color} />
+      </div>
+      <div className="display-tint-container">
+        {
+          tints.map((tint, idx) => (
+            <div className="swatch-small" key={idx}>
+              <Swatch hex={tint} size="small" />
+            </div>
+          ))
+        }
       </div>
     </div>
   );
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -7,3 +7,62 @@ export const createPageLis = (count) => {
 
   return pages;
 };
+
+export const hexToRgb = (hex) => {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+  return result ? {
+    r: Number.parseInt(result[1], 16),
+    g: Number.parseInt(result[2], 16),
+    b: Number.parseInt(result[3], 16)
+  } : null;
+};
+
+const numToHex = (num) => {
+  const hex = num.toString(16);
+
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+export const generateTint = (rgbObj, step) => {
+  let rgb = Object.assign({}, rgbObj);
+  let hex = '';
+
+  if (step > 0) {
+    if (rgb.r <= 255 - step) {
+      rgb.r += step;
+    }
+    if (rgb.g <= 255 - step) {
+      rgb.g += step;
+    }
+    if (rgb.b <= 255 - step) {
+      console.log(rgb.b);
+      rgb.b += step;
+    }
+  }
+  else if (step < 0) {
+    if (rgb.r >= 0 - step) {
+      rgb.r += step;
+    }
+    if (rgb.g >= 0 - step) {
+      rgb.g += step;
+    }
+    if (rgb.b >= 0 - step) {
+      rgb.b += step;
+    }
+    console.log(rgb, step);
+  }
+  else {
+    for (const channel in rgb) {
+      hex += numToHex(rgb[channel]);
+    }
+
+    return '#' + hex;
+  }
+
+  for (const channel in rgb) {
+    hex += numToHex(rgb[channel]);
+  }
+
+  return '#' + hex;
+}
